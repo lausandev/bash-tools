ff092533c0698168654011cdf3a29c275a95a0db
Will <wmanayaysanchez@gmail.com>
2026-07-13 16:16:46 -0400

    Add system information script

---
+++ system-info.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "==== Información del sistema ===="
+echo "Nombre del host: $hostname"
+echo "Versión: $(uname -r)"
+echo "Fecha: $(date '+%Y/%m/%d')"
+
+echo "================================="
