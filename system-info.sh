513cbb2188436a627c6e4bf8c5cce85815e6f142
Laulau <lsanchezramirez34@gmail.com>
2026-07-14 14:54:13 -0400

    Change system-info title

--- system-info.sh
+++ system-info.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 echo "==== Información del sistema ===="
+echo "==== SYSTEM INFO ======"
 echo "Nombre del host: $hostname"
 echo "Versión: $(uname -r)"
 echo "Fecha: $(date '+%Y/%m/%d')"
